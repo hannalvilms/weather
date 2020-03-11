81f031bdb36133d3ad77081f46ea2c733e29ac03
Hanna-Liisa Vilms <hanna-liisa.vilms@khk.ee>
2020-03-11 18:48:08 +0200

    Updates

---
+++ precache-manifest.2bd9b0cd662a9cd485831ffb7c039058.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a8e6cb90dec8b99a52efcfcd1238a2",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "3c1363a0595697760da2",
+    "url": "/weather/static/css/main.ec6929b8.chunk.css"
+  },
+  {
+    "revision": "60a8a7d72ef9bdf1510c",
+    "url": "/weather/static/js/2.5e0a622c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather/static/js/2.5e0a622c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c1363a0595697760da2",
+    "url": "/weather/static/js/main.286a1759.chunk.js"
+  },
+  {
+    "revision": "21baee1ee3fded119a76",
+    "url": "/weather/static/js/runtime-main.6859beea.js"
+  },
+  {
+    "revision": "8a81b5873220916b77ceff67cf3c109d",
+    "url": "/weather/static/media/cold.8a81b587.jpg"
+  },
+  {
+    "revision": "8591c1ca27ce095503e9416d9f834975",
+    "url": "/weather/static/media/rainy.8591c1ca.jpg"
+  },
+  {
+    "revision": "c56f90b3b7e841eae658f694c20e5c41",
+    "url": "/weather/static/media/snowy.c56f90b3.jpeg"
+  },
+  {
+    "revision": "69cb6bc9a9a20e1d8b2f1ea442997dc6",
+    "url": "/weather/static/media/sunny.69cb6bc9.jpeg"
+  }
+]);
